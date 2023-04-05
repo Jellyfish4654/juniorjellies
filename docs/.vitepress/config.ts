17e0dbe7d7980e9d9e477baf691cf9add2e0872e
Jeffrey <54014102+TheJeffreyKuo@users.noreply.github.com>
2023-04-05 02:31:03 -0400

    e

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Junior Jellies",
+  description: "Presented by FTC Team #4654 Jellyfish",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: nav(),
+    sidebar: {
+      '/java/': sidebarJava()
+    },
+
+  }
+})
+
+function nav() {
+  return [
+    { text: 'Guide', link: '/java/markdown-examples', activeMatch: '/java/' },
+  ]
+}
+
+function sidebarJava() {
+  return [
+    {
+      text: 'Java Lessons',
+      collapsed: false,
+      items: [
+        { text: 'Getting Started', link: '/java/markdown-examples' }
+      ]
+    },
+  ]
+}
---
+++ docs/index.md
@@ -0,0 +1,14 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "My Awesome Project"
+  text: "A VitePress Site"
+  tagline: My great project tagline
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /java/markdown-examples
+---
+
